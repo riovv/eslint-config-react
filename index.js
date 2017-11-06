2e20e894ccec97a5b003d59d614e0ae27f45a14a
Juan Miguel Rubio <jmrubio42@gmail.com>
2017-11-06 10:59:05 -0600

    Changes indent level to 4 spaces

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = {
     "parser": "babel-eslint",
 
     "parserOptions": {
-      "ecmaVersion": 6,
-      "ecmaFeatures": {
-        "jsx": true
-      },
-      "sourceType": "module"
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "sourceType": "module"
     },
 
     "plugins": [
